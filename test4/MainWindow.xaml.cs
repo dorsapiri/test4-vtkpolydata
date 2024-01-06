e60e7183ff52c79428f2c58e4302563dc705ce88
Dorsa <Dorsa@DESKTOP-8VT7FFQ>
2024-01-06 19:50:26 +0330

    Add project files.

---
+++ test4/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Kitware.VTK;
+
+namespace test4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int min;
+        int max;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowsFormsHost_Loaded(object sender, RoutedEventArgs e)
+        {
+            ///<summary>
+            ///Create Sphere
+            ///</summary>
+           
+            /*vtkSphereSource sphereSource = new vtkSphereSource();
+            sphereSource.SetRadius(100);
+            sphereSource.Update();
+
+            vtkPolyDataMapper SphereMapper = new vtkPolyDataMapper();
+            SphereMapper.SetInputData(sphereSource.GetOutput());
+            sphereSource.Update();
+
+            vtkActor sphereActor = new vtkActor();
+            sphereActor.SetMapper(SphereMapper);
+            //sphereActor.GetProperty().SetColor(0.5, 0.5, 0.5);
+
+            vRWC.RenderWindow.GetRenderers().GetFirstRenderer().AddActor(sphereActor);*/
+            
+
+            ///<summary>
+            ///show Dicom
+            ///</summary>
+            
+            vtkDICOMImageReader DicomImageReader = new vtkDICOMImageReader();
+            //DicomImageReader.SetDirectoryName(@"D:\DicomImages\A990410-BREAST\Study_1");
+            DicomImageReader.SetFileName(@"D:\DicomImages\A990410-BREAST\Study_1\CT_RAZEH GOUKEH^OZRA_S1_076.dcm");
+            DicomImageReader.Update();
+
+
+            var imageData = DicomImageReader.GetOutput();
+            
+            
+            
+
+            vtkPoints dicomPoints = new vtkPoints();
+            Read_Struct dicomRT = new Read_Struct();
+            var RTstructs = dicomRT.GetStructList(@"D:\DicomImages\A990410-BREAST\Study_1\RAZEH GOUKEH^OZRA__RTS.dcm");
+            var skin = RTstructs.First(x => x.Key.Equals("Skin")).Value;
+            
+            var slice99 = skin.SliceContours[99].ContourPoints;
+               
+            foreach (var point in slice99)
+                dicomPoints.InsertPoint(slice99.IndexOf(point), point.X, point.Y, skin.SliceContours[99].Z);
+
+            /*vtkPolyData polyData = new vtkPolyData();
+            polyData.SetPoints(dicomPoints);*/
+
+            vtkSelectPolyData skinloop = new vtkSelectPolyData();
+            skinloop.SetInputData(imageData);
+            //skinloop.SetInputConnection(0,imagepolydata);
+            skinloop.SetLoop(dicomPoints);
+            //skinloop.SetLoop(polyData.GetPoints());
+            skinloop.GenerateSelectionScalarsOn();
+            skinloop.SetSelectionModeToSmallestRegion();
+            
+
+            vtkClipPolyData clipPolyData = new vtkClipPolyData();
+            //clipPolyData.SetInputData(skinloop.GetOutput());
+            clipPolyData.SetInputConnection(skinloop.GetOutputPort());
+            //clipPolyData.Update();
+
+            vtkPolyDataMapper polyDataMapper = new vtkPolyDataMapper();
+            //polyDataMapper.SetInputData(clipPolyData.GetOutput());
+            polyDataMapper.SetInputConnection(clipPolyData.GetOutputPort());
+            //polyDataMapper.Update();
+
+            vtkLODActor polyActor = new vtkLODActor();
+            polyActor.SetMapper(polyDataMapper);
+
+            vRWC.RenderWindow.GetRenderers().GetFirstRenderer().AddActor(polyActor);
+            vRWC.RenderWindow.GetRenderers().GetFirstRenderer().SetBackground(0.1, 0.2, 0.4);
+            vRWC.RenderWindow.GetRenderers().GetFirstRenderer().ResetCamera();
+
+        }
+    }
+}
---
+++ test4/Read_Struct.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EvilDICOM.Core;
+using EvilDICOM.Core.Selection;
+using EvilDICOM.CV.Helpers;
+using EvilDICOM.CV.RT.Meta;
+using OpenCvSharp;
+
+namespace test4
+{
+    public class Read_Struct
+    {
+        public Read_Struct()
+        {
+            
+        }
+        public List<KeyValuePair<string, StructureMeta>> GetStructList(string File_Path)
+        {
+            DICOMSelector selector = DICOMObject.Read(File_Path).GetSelector();
+            StructureSetMeta structureSetMeta = new();
+            if (selector.Modality.Data != "RTSTRUCT") { return null; }
+
+            foreach (StructureMeta meta in selector.StructureSetROISequence.Items.Select((DICOMObject i) => new StructureMeta
+            {
+                StructureId = i.GetSelector().ROIName?.Data,
+                ROINumber = i.GetSelector().ROINumber.Data
+            }))
+            {
+                try
+                {
+                    DICOMObject dICOMObject = selector.ROIContourSequence.Items.FirstOrDefault((DICOMObject i) => i.GetSelector().ReferencedROINumber.Data == meta.ROINumber);
+                    DICOMObject? dICOMObject2 = selector.RTROIObservationsSequence.Items.FirstOrDefault((DICOMObject i) => i.GetSelector().ReferencedROINumber.Data == meta.ROINumber);
+                    List<int> data_ = dICOMObject.GetSelector().ROIDisplayColor.Data_;
+                    new Vec3b((byte)data_[0], (byte)data_[1], (byte)data_[2]);
+                    string data = dICOMObject2!.GetSelector().RTROIInterpretedType.Data;
+                    string data2 = (selector.ROIObservationLabel != null) ? selector.ROIObservationLabel.Data : " ";
+
+                    meta.StructureName = data2;
+                    meta.Color = new Scalar(data_[0], data_[1], data_[2]);
+                    if (dICOMObject.GetSelector().ContourSequence == null)
+                    {
+                        continue;
+                    }
+
+                    foreach (DICOMObject item in dICOMObject.GetSelector().ContourSequence.Items)
+                    {
+                        List<double> data_2 = item.GetSelector().ContourData.Data_;
+                        if (data_2.Count % 3 != 0)
+                        {
+                            //Logger.Error($"Slice for structure {meta.StructureId} has {data_2.Count} contour points. Not divisible by 3! Can't process.");
+                            continue;
+                        }
+
+                        try
+                        {
+                            SliceContourMeta sliceContourMeta = new SliceContourMeta();
+                            for (int j = 0; j < data_2.Count; j += 3)
+                            {
+                                Point3f pt = new Point3f((float)data_2[j], (float)data_2[j + 1], (float)data_2[j + 2]);
+                                sliceContourMeta.AddPoint(pt);
+                            }
+
+                            meta.SliceContours.Add(sliceContourMeta);
+                            meta.DICOMType = data;
+                        }
+                        catch (Exception ex)
+                        {
+                            //Logger.Error(ex.Message);
+                            Console.WriteLine(ex.ToString());
+                        }
+                    }
+
+                    foreach (IGrouping<float, SliceContourMeta> item2 in (from s in meta.SliceContours
+                                                                          group s by s.Z).ToList())
+                    {
+                        List<SliceContourMeta> list = item2.OrderByDescending((SliceContourMeta s) => s.CalculateArea()).ToList();
+                        ContourHelper.OrganizeIntoChildren(list[0], list.Skip(1));
+                    }
+                    structureSetMeta.Structures.Add(meta.StructureId, meta);
+                }
+                catch (Exception)
+                {
+                    //Logger.Error(exception.Message + "Could not add structure " + meta.StructureId.ToString());
+                }
+            }
+
+            if (structureSetMeta.Structures.Values.First().SliceContours.Count > 0)
+            {
+                return structureSetMeta.Structures.ToList();
+            }
+            else { return null; }
+
+        }
+    }
+}
