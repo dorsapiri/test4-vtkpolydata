cef959f78e0ccae1d5f6c11e9075279741f65784
dorsapiri <dorsapiri@gmail.com>
2024-01-17 16:20:04 +0330

    crop one dicom image!

--- test4/MainWindow.xaml.cs
+++ test4/MainWindow.xaml.cs
@@ -14,8 +14,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-
 using Kitware.VTK;
+//using OpenCvSharp;
 
 namespace test4
 {
@@ -47,15 +47,34 @@ namespace test4
             ///</summary>
 
             //DicomContour();
-            DrowSkinPolygon();
+            //DrowSkinPolygon();
+            CuttingDicom();
+
             /*ReadDICOMSeries();
             KeyDown += MainWindow_KeyDown;
             KeyUp += MainWindow_KeyUp;*/
 
-            //DicomOriginal();
+            //DicomView();
             //test();
 
         }
+
+        private void DicomView()
+        {
+            vtkDICOMImageReader DicomImageReader = new vtkDICOMImageReader();   
+            DicomImageReader.SetFileName(@"D:\DicomImages\A990410-BREAST\Study_1\CT_RAZEH GOUKEH^OZRA_S1_076.dcm");
+            DicomImageReader.Update();
+
+            vtkImageActor imageActor = new vtkImageActor();
+            imageActor.SetInputData(DicomImageReader.GetOutput());
+
+            vRWC.RenderWindow.GetRenderers().GetFirstRenderer().AddActor(imageActor);
+            vRWC.RenderWindow.GetRenderers().GetFirstRenderer().SetBackground(.5, .5, .5);
+            vRWC.RenderWindow.GetRenderers().GetFirstRenderer().ResetCamera();
+
+           
+        }
+
         public void DicomContour() 
         {
             vtkDICOMImageReader DicomImageReader = new vtkDICOMImageReader();
@@ -110,12 +129,15 @@ namespace test4
             var slice100 = skin.SliceContours[99].ContourPoints;
             var slice1 = skin.SliceContours[0].ContourPoints;
             var zCoordination = skin.SliceContours[0].Z;
+
+            var xMin = slice1.Min(p => p.X);
+            var yMin = slice1.Min(p => p.Y);
             //VtkPoints add Contour Points 
             vtkPoints curvePoints = new vtkPoints();
             foreach (var point in slice1) 
             {
-                var x = point.X;
-                var y = point.Y;
+                var x = point.X - xMin;
+                var y = point.Y - yMin;
                 curvePoints.InsertNextPoint(x, y, zCoordination);
             }
             
@@ -129,7 +151,8 @@ namespace test4
             
             vtkPolyData polyData = new vtkPolyData();
             polyData.SetPoints(curvePoints);
-            polyData.SetPolys(curveCells);
+            //polyData.SetPolys(curveCells);
+            polyData.SetLines(curveCells);
 
             vtkPolyDataMapper mapper = new vtkPolyDataMapper();
             mapper.SetInputData(polyData);
@@ -147,115 +170,109 @@ namespace test4
 
 
         }
-        public void SphereProcessing()
+        
+
+        public void CuttingDicom()
         {
-            vtkSphereSource sphereSource = vtkSphereSource.New();
-            sphereSource.Update();
+            vtkDICOMImageReader DicomImage = new vtkDICOMImageReader();
+            DicomImage.SetFileName(@"D:\DicomImages\A990410-BREAST\Study_1\CT_RAZEH GOUKEH^OZRA_S1_076.dcm");
+            //DicomImageReader.SetDirectoryName(@"D:\DicomImages\A990410-BREAST\Study_1");
+            DicomImage.Update();
 
-            vtkPoints selectionPoints = vtkPoints.New();
+            /*vtkImageData imageData = DicomImage.GetOutput();
 
-            selectionPoints.InsertPoint(0, -0.16553, 0.135971, 0.451972);
-            selectionPoints.InsertPoint(1, -0.0880123, -0.134952, 0.4747);
-            selectionPoints.InsertPoint(2, 0.00292618, -0.134604, 0.482459);
-            selectionPoints.InsertPoint(3, 0.0641941, 0.067112, 0.490947);
-            selectionPoints.InsertPoint(4, 0.15577, 0.0734765, 0.469245);
-            selectionPoints.InsertPoint(5, 0.166667, -0.129217, 0.454622);
-            selectionPoints.InsertPoint(6, 0.241259, -0.123363, 0.420581);
-            selectionPoints.InsertPoint(7, 0.240334, 0.0727106, 0.432555);
-            selectionPoints.InsertPoint(8, 0.308529, 0.0844311, 0.384357);
-            selectionPoints.InsertPoint(9, 0.32672, -0.121674, 0.359187);
-            selectionPoints.InsertPoint(10, 0.380721, -0.117342, 0.302527);
-            selectionPoints.InsertPoint(11, 0.387804, 0.0455074, 0.312375);
-            selectionPoints.InsertPoint(12, 0.43943, -0.111673, 0.211707);
-            selectionPoints.InsertPoint(13, 0.470984, -0.0801913, 0.147919);
-            selectionPoints.InsertPoint(14, 0.436777, 0.0688872, 0.233021);
-            selectionPoints.InsertPoint(15, 0.44874, 0.188852, 0.109882);
-            selectionPoints.InsertPoint(16, 0.391352, 0.254285, 0.176943);
-            selectionPoints.InsertPoint(17, 0.373274, 0.154162, 0.294296);
-            selectionPoints.InsertPoint(18, 0.274659, 0.311654, 0.276609);
-            selectionPoints.InsertPoint(19, 0.206068, 0.31396, 0.329702);
-            selectionPoints.InsertPoint(20, 0.263789, 0.174982, 0.387308);
-            selectionPoints.InsertPoint(21, 0.213034, 0.175485, 0.417142);
-            selectionPoints.InsertPoint(22, 0.169113, 0.261974, 0.390286);
-            selectionPoints.InsertPoint(23, 0.102552, 0.25997, 0.414814);
-            selectionPoints.InsertPoint(24, 0.131512, 0.161254, 0.454705);
-            selectionPoints.InsertPoint(25, 0.000192443, 0.156264, 0.475307);
-            selectionPoints.InsertPoint(26, -0.0392091, 0.000251724, 0.499943);
-            selectionPoints.InsertPoint(27, -0.096161, 0.159646, 0.46438);
+            vtkContourFilter contourFilter = new vtkContourFilter();
+            contourFilter.SetInputData(DicomImage.GetOutput());
 
-            vtkSelectPolyData loop = vtkSelectPolyData.New();
-            loop.SetInputConnection(sphereSource.GetOutputPort());
-            loop.SetLoop(selectionPoints);
-            loop.GenerateSelectionScalarsOn();
-            loop.SetSelectionModeToSmallestRegion(); //negative scalars inside
+            vtkPolyData contourPolyData = contourFilter.GetOutput();
+            contourFilter.Update();*/
 
-            vtkClipPolyData clip = //clips out positive region
-            vtkClipPolyData.New();
-            clip.SetInputConnection(loop.GetOutputPort());
+            vtkImageViewer2 dicomViewer = new vtkImageViewer2();
+            
+            var imageCenter = DicomImage.GetOutput().GetCenter();
 
-            vtkPolyDataMapper clipMapper = vtkPolyDataMapper.New();
-            clipMapper.SetInputConnection(clip.GetOutputPort());
-
-            vtkLODActor clipActor = vtkLODActor.New();
-            clipActor.SetMapper(clipMapper);
-
-            vRWC.RenderWindow.GetRenderers().GetFirstRenderer().AddActor(clipActor);
-        }
-
-        public void DicomOriginal()
-        {
-            vtkDICOMImageReader DicomImageReader = new vtkDICOMImageReader();
-            DicomImageReader.SetFileName(@"D:\DicomImages\A990410-BREAST\Study_1\CT_RAZEH GOUKEH^OZRA_S1_076.dcm");
-            DicomImageReader.Update();
-
-            //Get Contour
+            //Contour Points in RT-Structure
             Read_Struct dicomRT = new Read_Struct();
             var RTstructs = dicomRT.GetStructList(@"D:\DicomImages\A990410-BREAST\Study_1\RAZEH GOUKEH^OZRA__RTS.dcm");
             var skin = RTstructs.First(x => x.Key.Equals("Skin")).Value;
-            var slice99 = skin.SliceContours[99].ContourPoints;
-         
-            //show Contours
+            var slice1 = skin.SliceContours[0].ContourPoints;
+            var zCoordination = skin.SliceContours[0].Z;
+            var xMin = slice1.Min(p => p.X);
+            var xMax = slice1.Max(p => p.X);
+            var yMin = slice1.Min(p => p.Y);
+            var yMax = slice1.Max(p => p.Y);
+            var xCenter = (xMax - xMin) / 2;
+            var yCenter = (yMax - yMin) / 2;
+            var diffCenterX = imageCenter[0] - xCenter;
+            var diffCenterY = imageCenter[1] - yCenter;
+            //VtkPoints add Contour Points 
             vtkPoints curvePoints = new vtkPoints();
-            foreach (var point in slice99)
-                curvePoints.InsertPoint(slice99.IndexOf(point), point.X, point.Y, skin.SliceContours[99].Z);
+            foreach (var point in slice1)
+            {
+                var x = point.X - xMin + diffCenterX;
+                var y = point.Y - yMin + diffCenterY;
+                curvePoints.InsertNextPoint(x, y, zCoordination);
+            }
+
+            vtkCellArray curveCells = new vtkCellArray();
+            var lastPid = slice1.IndexOf(slice1.Last());
+            curveCells.InsertNextCell(lastPid);
+            for (var point = 0; point < lastPid; point++)
+            {
+                curveCells.InsertCellPoint(point);
+            }
+
+            vtkPolyData polyData = new vtkPolyData();
+            polyData.SetPoints(curvePoints);
+            polyData.SetLines(curveCells);
+
+            vtkImageData newImageData = new vtkImageData();
+            vtkImageData imageData = DicomImage.GetOutput();
+            var test = imageData.GetPointData().GetScalars().GetTuple1(131328);
+            var fpointPosition = imageData.GetPoint(0);
+            var lpointPosition = imageData.GetPoint(512*512-1);
+            var minXcurve = polyData.GetBounds()[0];
+            var maxXcurve = polyData.GetBounds()[1];
+            var minYcurve = polyData.GetBounds()[2];
+            var maxYcurve = polyData.GetBounds()[3];
             
-            vtkSelectPolyData curve = vtkSelectPolyData.New();
-            curve.SetInputConnection(DicomImageReader.GetOutputPort());
-            curve.SetLoop(curvePoints);
-            curve.GenerateSelectionScalarsOn();
-            //curve.SetSelectionModeToLargestRegion();
-            curve.SetSelectionModeToSmallestRegion();
+
+            double[] croppingRegion = { minXcurve, maxXcurve, minYcurve, maxYcurve, 0, 0 };
             
+            int[] dims = DicomImage.GetOutput().GetDimensions();
+            double[] spacing = DicomImage.GetPixelSpacing();
+            float[] origin = DicomImage.GetImagePositionPatient();
 
-            vtkClipPolyData clipCurve = vtkClipPolyData.New();
-            clipCurve.SetInputConnection(curve.GetOutputPort());
+            //the region of interest (ROI)
+            int roiXcoord = (int)Math.Round(minXcurve); 
+            int roiYCoord = (int)Math.Round(minYcurve); 
+            int roiWidth = (int)Math.Round(maxXcurve); 
+            int roiHeight = (int)Math.Round(maxYcurve); 
 
-            vtkPolyDataMapper curveMapper = vtkPolyDataMapper.New();
-            curveMapper.SetInputData(clipCurve.GetOutput());
+            // Create a region of interest filter
+            vtkExtractVOI roiFilter = vtkExtractVOI.New();
+            roiFilter.SetInputConnection(DicomImage.GetOutputPort());
+            roiFilter.SetVOI(roiXcoord, roiXcoord + roiWidth - 1, roiYCoord, roiYCoord + roiHeight - 1, 0, 0);
+            roiFilter.Update();
 
-            vtkLODActor actor = new vtkLODActor();
-            actor.GetProperty().SetColor(0, 1, 0);
-            actor.SetMapper(curveMapper);
-            
+            vtkImageActor imageActor = vtkImageActor.New();
+            imageActor.SetInputData(roiFilter.GetOutput());
 
-            vRWC.RenderWindow.GetRenderers().GetFirstRenderer().AddActor(actor);
-            vRWC.RenderWindow.GetRenderers().GetFirstRenderer().Render();
-            vRWC.RenderWindow.GetRenderers().GetFirstRenderer().SetBackground(.5, .5, .5);
+            vtkCamera camera = vRWC.RenderWindow.GetRenderers().GetFirstRenderer().GetActiveCamera();
+            camera.SetPosition(origin[0], origin[1], origin[2] + spacing[2] * dims[2]);
+            camera.SetFocalPoint(origin[0], origin[1], origin[2]);
+            camera.SetViewUp(0, 1, 0);
+            //camera.Azimuth(30.0);
+            //camera.Elevation(30.0);
+
+            vRWC.RenderWindow.GetRenderers().GetFirstRenderer().AddActor(imageActor);
             vRWC.RenderWindow.GetRenderers().GetFirstRenderer().ResetCamera();
-
             
-           
-
-            /*vtkImageViewer2 dicomViewer = new vtkImageViewer2();
-            dicomViewer.SetInputData(DicomImageReader.GetOutput());
-            //dicomViewer.GetRenderer().AddActor(actor);
-            dicomViewer.SetupInteractor(renderWindowInteractor);
-            dicomViewer.SetRenderWindow(vRWC.RenderWindow);
-            dicomViewer.Render();
-            renderWindowInteractor.Start();*/
-
+            
         }
 
+
+        //-------------------------Test Area-------------------
         vtkImageViewer2 _ImageViewer;
         vtkTextMapper _SliceStatusMapper;
         int _Slice;
@@ -431,7 +448,60 @@ namespace test4
         }*/
 
 
-       
+        public void SphereProcessing()
+        {
+            vtkSphereSource sphereSource = vtkSphereSource.New();
+            sphereSource.Update();
+
+            vtkPoints selectionPoints = vtkPoints.New();
+
+            selectionPoints.InsertPoint(0, -0.16553, 0.135971, 0.451972);
+            selectionPoints.InsertPoint(1, -0.0880123, -0.134952, 0.4747);
+            selectionPoints.InsertPoint(2, 0.00292618, -0.134604, 0.482459);
+            selectionPoints.InsertPoint(3, 0.0641941, 0.067112, 0.490947);
+            selectionPoints.InsertPoint(4, 0.15577, 0.0734765, 0.469245);
+            selectionPoints.InsertPoint(5, 0.166667, -0.129217, 0.454622);
+            selectionPoints.InsertPoint(6, 0.241259, -0.123363, 0.420581);
+            selectionPoints.InsertPoint(7, 0.240334, 0.0727106, 0.432555);
+            selectionPoints.InsertPoint(8, 0.308529, 0.0844311, 0.384357);
+            selectionPoints.InsertPoint(9, 0.32672, -0.121674, 0.359187);
+            selectionPoints.InsertPoint(10, 0.380721, -0.117342, 0.302527);
+            selectionPoints.InsertPoint(11, 0.387804, 0.0455074, 0.312375);
+            selectionPoints.InsertPoint(12, 0.43943, -0.111673, 0.211707);
+            selectionPoints.InsertPoint(13, 0.470984, -0.0801913, 0.147919);
+            selectionPoints.InsertPoint(14, 0.436777, 0.0688872, 0.233021);
+            selectionPoints.InsertPoint(15, 0.44874, 0.188852, 0.109882);
+            selectionPoints.InsertPoint(16, 0.391352, 0.254285, 0.176943);
+            selectionPoints.InsertPoint(17, 0.373274, 0.154162, 0.294296);
+            selectionPoints.InsertPoint(18, 0.274659, 0.311654, 0.276609);
+            selectionPoints.InsertPoint(19, 0.206068, 0.31396, 0.329702);
+            selectionPoints.InsertPoint(20, 0.263789, 0.174982, 0.387308);
+            selectionPoints.InsertPoint(21, 0.213034, 0.175485, 0.417142);
+            selectionPoints.InsertPoint(22, 0.169113, 0.261974, 0.390286);
+            selectionPoints.InsertPoint(23, 0.102552, 0.25997, 0.414814);
+            selectionPoints.InsertPoint(24, 0.131512, 0.161254, 0.454705);
+            selectionPoints.InsertPoint(25, 0.000192443, 0.156264, 0.475307);
+            selectionPoints.InsertPoint(26, -0.0392091, 0.000251724, 0.499943);
+            selectionPoints.InsertPoint(27, -0.096161, 0.159646, 0.46438);
+
+            vtkSelectPolyData loop = vtkSelectPolyData.New();
+            loop.SetInputConnection(sphereSource.GetOutputPort());
+            loop.SetLoop(selectionPoints);
+            loop.GenerateSelectionScalarsOn();
+            loop.SetSelectionModeToSmallestRegion(); //negative scalars inside
+
+            vtkClipPolyData clip = //clips out positive region
+            vtkClipPolyData.New();
+            clip.SetInputConnection(loop.GetOutputPort());
+
+            vtkPolyDataMapper clipMapper = vtkPolyDataMapper.New();
+            clipMapper.SetInputConnection(clip.GetOutputPort());
+
+            vtkLODActor clipActor = vtkLODActor.New();
+            clipActor.SetMapper(clipMapper);
+
+            vRWC.RenderWindow.GetRenderers().GetFirstRenderer().AddActor(clipActor);
+        }
         public void test()
         {
             vtkPoints points = vtkPoints.New();
